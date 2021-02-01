66b4799bf9f2e8374e81d52b885ec92fe341b3c4
john <jonwanjema@gmail.com>
2021-02-01 18:18:55 +0300

    add code for blinking led

--- sketch_jan27a/led/led.ino
+++ sketch_jan27a/led/led.ino
@@ -10,9 +10,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH); 
-
+  Serial.println("led on");
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);
+   Serial.println("led of");
     // turn the LED off by making the voltage LOW
   delay(1000);
 }
